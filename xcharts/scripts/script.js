1325b6172b78e8b1764c67691bd504e7de225a99
Mike Henry <mhenry2@sapient.com>
2014-10-30 15:33:25 -0400

    Added xCharts scatterplot vis type and demo

--- xcharts/scripts/script.js
+++ xcharts/scripts/script.js
@@ -1,56 +1,98 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-var data = {
-  "xScale": "ordinal",
-  "yScale": "linear",
-  "main": [
-    {
-      "className": ".pizza",
-      "data": [
-        {
-          "x": "Pepperoni",
-          "y": 4
-        },
-        {
-          "x": "Cheese",
-          "y": 8
-        },
-        {
-          "x": "Sausage",
-          "y": 5
-        },
-        {
-          "x": "Veggie Supreme",
-          "y": 3
-        }
-      ]
-    },
-    {
-      "className": ".pizza2",
-      "data": [
-        {
-          "x": "Pepperoni",
-          "y": 6
-        },
-        {
-          "x": "Cheese",
-          "y": 5
-        },
-        {
-          "x": "Sausage",
-          "y": 8
-        },
-        {
-          "x": "Veggie Supreme",
-          "y": 2
-        }
-      ]
+	function generateRandomScatter(min, max, n) {
+		var data = [];
+		for (var i = 0; i < n; i++) {
+			data.push({
+				"x":getRandom(min,max),
+				"y":getRandom(min,max)
+			});
+		}
+		return data;
+	}
+
+	function getRandom (min, max) {
+        return Math.random() * (max - min + 1) + min;
     }
-  ]
-};
 
-  var myChart = new xChart('cumulative', data, '#bar-chart');
+	var data = {
+		"xScale": "linear",
+		"yScale": "linear",
+		"main": [
+			{
+				"className": ".freshmen",
+				"data": generateRandomScatter(20,100,200)
+			},
+			{
+				"className": ".sophomore",
+				"data": generateRandomScatter(20,100,200)
+			},
+			{
+				"className": ".junior",
+				"data": generateRandomScatter(20,100,200)
+			},
+			{
+				"className": ".senior",
+				"data": generateRandomScatter(20,100,200)
+			}
+		]
+	};
 
-  
+
+	var scatter = {
+		enter: function(self, storage, className, data) {
+
+			var container = self._g.selectAll('g' + className)
+			.data(data, function (d) {
+				return d.className;
+			});
+
+			container
+			.enter()
+			.append('g')
+			.attr('class', function (d, i) {
+				return 'circle' + className.replace(/\./g, ' ') +
+					' color' + i;
+			});
+
+			var circles = container.selectAll('circle')
+				.data(function (d) {
+					return d.data;
+				});
+
+			circles.enter()
+				.insert('circle')
+				.attr('style',"stroke-width: 0")
+				.attr('r',5)
+				.attr('cx',function(d) { return self.xScale(d.x)})
+				.attr('cy',function(d) { return self.yScale(d.y)});
+
+			storage.containers = container;
+			storage.circles = circles;
+
+		},
+		update: function(self, storage, timing) {
+			storage.circles.transition().duration(timing)
+				.style('opacity', 1)
+				.attr('cx',function(d) { return self.xScale(d.x + getRandom(-10,10))})
+				.attr('cy',function(d) { return self.yScale(d.y + getRandom(-10,10))});
+		},
+		exit: function(self, storage, timing) {
+			storage.circles.exit()
+				.transition().duration(timing)
+				.style('opacity', 0);
+		},
+		destroy: function(self, storage, timing) {
+
+			storage.circles
+				.transition().duration(timing)
+				.style('opacity', 0)
+				.remove();
+		}
+	};
+
+	xChart.setVis('scatter', scatter);
+
+	var myChart = new xChart('scatter', data, '#bar-chart');
 });
