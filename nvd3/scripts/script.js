e064d814ecb18f8c72502d125cf5fa4b5bd36725
rculp <rlculp3@gmail.com>
2014-11-12 11:27:37 -0500

    Added cross filter and dc.js to project

--- nvd3/scripts/script.js
+++ nvd3/scripts/script.js
@@ -1,9 +1,11 @@
 function getDataSeries(data) {
-    var result = [{"key": "Allocation", "values": []}, {"key": "1-Day Returns", "values": []}, {"key": "1-Year Returns", "values": []}];
+    var result = [{"key": "Allocation", "values": []}, 
+        {"key": "1-Day Returns", "values": []}, 
+        {"key": "1-Year Returns", "values": []}];
     for(var i = 0; i < data.length -1; i++) {
-        result[0].values.push({x: data[i].sector, y: data[i].allocation});
-        result[1].values.push({x: data[i].sector, y: data[i].return1d});
-        result[2].values.push({x: data[i].sector, y: data[i].return1yr});
+        result[0].values.push({x: data[i].sectorCode, y: data[i].allocation});
+        result[1].values.push({x: data[i].sectorCode, y: data[i].return1d});
+        result[2].values.push({x: data[i].sectorCode, y: data[i].return1yr});
     }
     console.log(result);
     return result;
@@ -14,14 +16,22 @@ d3.json("data.json",function(datafile){
 
     nv.addGraph(function() {
         var chart = nv.models.multiBarHorizontalChart()
-            .width(600)
+            //.width(600)
             .height(400)
             //.tooltips(false)
             .stacked(true)
             .showControls(false);
         d3.select("#chart").append("svg")
             .datum(getDataSeries(data))
-            .transition().duration(500).call(chart).style({ 'width': '700', 'height': '400', 'padding-left':'100px' });
+            .transition().duration(500)
+            .call(chart).style({'height': '400' });
+
+        nv.utils.windowResize(
+            function() {
+                chart.update();
+            }
+        );
+
 
         return chart;
     });
